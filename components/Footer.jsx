49bd535225883c3c0e676cc01b3ae83ad06b708d
NigarRahimli <nigarrahimli07@gmail.com>
2023-10-23 19:13:36 +0400

    home is ready

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+<div className="bg-[#212529] flex justify-between px-[100px] py-[40px] text-white ">
+  <h1>VENCE Studio.</h1>
+  <div className="flex gap-x-5 ">
+    <p>About Studio</p>
+    <p>Archive</p>
+    <p>Contact Us</p>
+  </div>
+  
+</div>
+<hr className='text-white' />
+
+   
+
+    </div>
   )
 }
 
